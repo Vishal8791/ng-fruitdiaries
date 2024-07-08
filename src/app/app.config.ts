b755314c57fb735285f2a1d2303f554aa1489c02
VishalSharma8791 <121858947+VishalSharma8791@users.noreply.github.com>
2024-07-08 12:29:12 +0530

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideClientHydration()
   ]
 };
